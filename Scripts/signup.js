6e6317a775354582bffb69252ae2e6451d57c9b4
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-28 18:55:01 +0300

    modifying script to check if username already exists

--- Scripts/signup.js
+++ Scripts/signup.js
@@ -3,6 +3,8 @@ let signup=document.getElementById('signup');
 signup.addEventListener('click', addUser);
 
 function addUser(){
+
+
     let fname=document.getElementById('fname').value;
     let lastname=document.getElementById('lname').value;
     let email=document.getElementById('email').value;
@@ -10,9 +12,25 @@ function addUser(){
     let gender=document.getElementById('gender-types').value;
     
     if (fname=='' || lastname=='' || email=='' || password=='' || gender==''){
-        window.alert('These fields are required')
+        window.alert('These fields are required');
     } else {
-        let data = new FormData();
+      
+            let data =new FormData();
+            data.append('email',email);
+            data.append('password',password);
+            axios({
+                method:'post',
+                url:'http://localhost/foodex-group-project-back-end/login.php',
+                data:data
+            }).then(function(response){
+                if (response.data['response']=="logged in"){
+                    console.log(response);
+                    console.log(response.data['user_id']);
+                    window.alert('username and/or password already exist')
+                }
+                else {
+                   
+                    let data = new FormData();
 
         data.append('email',email);
         data.append('password',password);
@@ -25,14 +43,16 @@ function addUser(){
             url:'http://localhost/foodex-group-project-back-end/signup.php',
             data:data
         }).then(function(response){
-            if (response.data['success']){
+           
                 window.location.href='http://localhost/foodex-group-project-front-end/index.html';
                 console.log(response.data['success']);
-            }else {
-                window.alert('email and/or password already exists')
-            }
+           
                
             })
+                }
+            })
+        
+        
     }
 
 
