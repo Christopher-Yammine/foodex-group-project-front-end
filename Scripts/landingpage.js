934b07772ba64a30e93c97fb7d2d0199a8713354
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-29 19:02:57 +0300

    creating restaurants' page with scripts

--- Scripts/landingpage.js
+++ Scripts/landingpage.js
@@ -1,10 +1,10 @@
-window.onload=function(){
+window.onload = function(){
 let fname = sessionStorage.getItem("name");
 
 let greeting =document.getElementById('greet');
 greeting.innerHTML='We are glad you made it ' + fname + '!' ;
 
-var restau=document.getElementById('restau-body');
+/*var restau=document.getElementById('restau-body');
 console.log(restau);
 var row='';
 var n=1
@@ -18,6 +18,61 @@ for( let i=0 ;i<2;i++){
     restau.innerHTML+='<div class="restau-row">'+ row+ '</div>';
     row='';
 }
+let div0=document.getElementsByClassName('restau-div');
+console.log(div0);
+for (let i=0;i<div0.length;i++){
+    console.log(div0);
+    div0[i].addEventListener('click',function(event){
+        console.log(event.currentTarget.id);
+    })
+}*/
 
 
+let restauBody=document.getElementById('restau-body');
+    restauBody.innerHTML='';
+    let data=new FormData();
+
+    axios({
+        method:'get',
+        url:'http://localhost/foodex-group-project-back-end/landingpage.php',
+        data:data
+    }).then (function(response){
+        console.log(Math.ceil(response.data.length/3));
+        let n=1;
+        var row='';
+        for (let i=0;i<Math.ceil(response.data.length/3);i++){
+            for (let j=n;j<response.data.length+1;++j){
+                row+=`<div class="restau-div" id=${response.data[j-1].id_restaurant}>
+                <img src="../CSS/assets/foodbg.jpg" alt="../CSS/assets/foodbg.jpg">
+                <div class="inner-title">
+                    <h4>${response.data[j-1].restaurant_name}</h4>
+                    <h4>${response.data[j-1].city_name},${response.data[j-1].restaurant_number}</h4>
+                </div>
+                <div class="description">${response.data[j-1].restaurant_description}
+                </div>
+                <div class="type">
+                ${response.data[j-1].type}
+                </div> 
+            </div>`;
+            if (j%3==0) {
+                n=j+1;
+                break;
+                } 
+            }
+            restauBody.innerHTML+='<div class="restau-row">'+row+'</div>';
+            row='';
+        }
+        let div0=document.getElementsByClassName('restau-div');
+        console.log(div0);
+        for (let i=0;i<div0.length;i++){
+        console.log(div0);
+        div0[i].addEventListener('click',function(event){
+        window.location.href='http://localhost/foodex-group-project-front-end/html/reviews.html?id='+event.currentTarget.id;
+        
+    })}
+    })
+    
+    
 }
+
+//${response.data[j-1].restaurant_picture}
