5bd0665b683e5a6a0872e55cf3513be79b1ccbd9
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-31 01:54:28 +0300

    adding admin panel

---
+++ Scripts/adminpanel.js
@@ -0,0 +1,17 @@
+let base64String = "";
+function imageUploaded() {
+	var file = document.querySelector(
+		'input[type=file]')['files'][0];
+
+	var reader = new FileReader();
+	console.log("next");
+	
+	reader.onload = function () {
+		base64String = reader.result;
+		var disp=document.getElementById('display');
+        disp.innerHTML='';
+		disp.innerHTML=`<img src="`+base64String+`">`
+	}
+	reader.readAsDataURL(file);
+
+}    
