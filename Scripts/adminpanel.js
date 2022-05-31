4cf95be6758592a625497b7c1045cbfc496a747a
ChristopherYammine <chrisyammine98@gmail.com>
2022-05-31 19:01:13 +0300

    Linking the form to the api

--- Scripts/adminpanel.js
+++ Scripts/adminpanel.js
@@ -27,10 +27,9 @@ window.onload=function(){
 	let btn=document.getElementById('btn');
 	let locbtn=document.getElementById('locbtn');
 	let typebtn=document.getElementById('typebtn');
-	function cardboardDiv(){
-		
 	
-
+	
+	function cardboardDiv(){
 
 	let data=new FormData();
 	axios({
@@ -57,7 +56,7 @@ window.onload=function(){
 		url:'http://localhost/foodex-group-project-back-end/types.php',
 		data:data1
 	}).then(function(response){
-		console.log(response);
+		
 		let types=document.getElementById('types');
 		types.innerHTML=''
 		let options=''
@@ -67,6 +66,8 @@ window.onload=function(){
 		types.innerHTML+=options;
 		
 	})
+	
+
 	}
 	cardboardDiv();
 
@@ -87,13 +88,23 @@ window.onload=function(){
 			url:'http://localhost/foodex-group-project-back-end/addlocation.php',
 			data:data
 		}).then(function(response){
-			console.log(response);
+			
 			cardboardDiv();
 		})
 
 	}
 	function addtype(){
-		console.log("also working");
+		let newtype=document.getElementById('newtype').value;
+		let data=new FormData();
+		data.append("type",newtype);
+		axios({
+			method:'post',
+			url:'http://localhost/foodex-group-project-back-end/addtype.php',
+			data:data
+		}).then(function(response){
+			cardboardDiv();
+			
+		})
 	}
 
 	
@@ -101,10 +112,31 @@ window.onload=function(){
 
 	function addRestau(){
 		let dispPic=base64String;
-		let restaurant_name=document.getElementById('restaurant_name');
-		let restaurant_description=document.getElementById('restaurant_description');
-		let restaurant_number=document.getElementById('restaurant_number');
-		console.log(dispPic);
+		let restaurant_name=document.getElementById('restaurant_name').value;
+		let restaurant_description=document.getElementById('restaurant_description').value;
+		let restaurant_number=document.getElementById('restaurant_number').value;
+		let locations=document.getElementById('locations').value;
+		let types=document.getElementById('types').value;
+		if(dispPic=='' || restaurant_name=='' || restaurant_description=='' ||restaurant_number==''){
+			window.alert('fields are missing');
+		} else {
+			let data =new FormData();
+			data.append("restaurant_name",restaurant_name);
+			data.append("restaurant_description",restaurant_description);
+			data.append("restaurant_number",restaurant_number);
+			data.append("restaurant_type",types);
+			data.append("restaurant_picture",dispPic);
+			data.append("location_id",locations);
+			axios({
+				method:'post',
+				url:'http://localhost/foodex-group-project-back-end/addrestau.php',
+				data:data
+			}).then(function(response){
+				console.log(response);
+				window.location.reload();
+			})
+		}
+		console.log(dispPic,restaurant_name,restaurant_description,restaurant_number,locations,types);
 	}
 
 
